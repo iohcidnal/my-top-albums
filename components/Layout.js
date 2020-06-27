45d2f902db626a7370a871548f0eeb4618788243
Ian Landicho <landicho.ian@gmail.com>
2020-06-27 12:52:41 -0600

    Add Layout component

---
+++ components/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Layout(props) {
+  return (
+    <React.Fragment>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
+        <title>My Top Albums</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <section className="section">
+        <div className="container">{props.children}</div>
+      </section>
+    </React.Fragment>
+  );
+}
