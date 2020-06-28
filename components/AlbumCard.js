2b303ebd8e2249da30f613904c9e9df06461e51c
Ian Landicho <landicho.ian@gmail.com>
2020-06-27 20:47:15 -0600

    Add view album info feature

--- components/AlbumCard.js
+++ components/AlbumCard.js
@@ -1,34 +1,102 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import fetch from 'node-fetch';
+
+async function fetchAlbum(albumHref) {
+  const accessToken = localStorage.getItem('spotify-access-token');
+  const options = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+  const fetchResult = await fetch(albumHref, options);
+  const jsonResult = await fetchResult.json();
+  // TODO: handle expired token
+
+  return jsonResult;
+}
 
 function AlbumCard({ album }) {
   console.log('AlbumCard');
+  const [info, setInfo] = React.useState();
+
+  async function handleGetInfo() {
+    const result = await fetchAlbum(album.href);
+    setInfo(result);
+  }
+
   return (
-    <div className="column is-one-quarter card">
-      <div className="card-image">
-        <figure>
-          <img src={album.images[1].url} alt="Placeholder image" />
-        </figure>
-      </div>
-      <div className="card-content">
-        <div className="media-content">
-          <p className="title is-4">{album.name}</p>
-          <p className="subtitle is-6">{album.artists[0].name}</p>
+    <React.Fragment>
+      <div className="column is-one-quarter card">
+        <div className="card-image">
+          <figure>
+            <img src={album.images[1].url} alt="Album image" />
+          </figure>
         </div>
-        <div className="buttons">
-          <button className="button is-light">
-            <span className="icon">
-              <i className="fas fa-info" />
-            </span>
-          </button>
-          <button className="button is-light">
-            <span className="icon">
-              <i className="fas fa-heart" />
-            </span>
-          </button>
+        <div className="card-content">
+          <div className="media-content">
+            <p className="title is-4">{album.name}</p>
+            <p className="subtitle is-6">{album.artists[0].name}</p>
+          </div>
+          <div className="buttons">
+            <button className="button is-light" onClick={handleGetInfo}>
+              <span className="icon">
+                <i className="fas fa-info" />
+              </span>
+            </button>
+            <button className="button is-light">
+              <span className="icon">
+                <i className="fas fa-heart" />
+              </span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      {info && (
+        <div className="modal is-active">
+          <div className="modal-background"></div>
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <p className="modal-card-title">{album.name}</p>
+            </header>
+            <div className="modal-card-body">
+              <div className="columns">
+                <div className="column">
+                  <div className="box">
+                    <figure>
+                      <img src={album.images[1].url} alt="Album image" />
+                    </figure>
+                  </div>
+                </div>
+                <div className="column">
+                  <div className="columns">
+                    <div className="column">Artist</div>
+                    <div className="column">{album.artists[0].name}</div>
+                  </div>
+                  <div className="columns">
+                    <div className="column">Release date</div>
+                    <div className="column">{album.release_date}</div>
+                  </div>
+                  <div className="content">
+                    Tracks
+                    <ul>
+                      {info.tracks.items.map(track => (
+                        <li key={track.id}>{track.name}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <footer className="modal-card-foot">
+              <button className="button" onClick={() => setInfo(null)}>
+                Close
+              </button>
+            </footer>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
   );
 }
 
