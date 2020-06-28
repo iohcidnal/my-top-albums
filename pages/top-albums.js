33c59149e184928d388dededc821f31a0b8ea5ed
Ian Landicho <landicho.ian@gmail.com>
2020-06-27 22:26:32 -0600

    Replace tabs with nav
    Use sticky navigation instead of tabs.

--- pages/top-albums.js
+++ pages/top-albums.js
@@ -2,34 +2,51 @@ import React from 'react';
 import { BrowseArtists, Layout, TopAlbumsProvider } from '../components';
 
 const BROWSE = 'Browse';
-const MY_TOP_ALBUMS = 'My Top Albums';
+const MY_TOP_10_ALBUMS = 'My Top 10 Albums';
 
 export default function TopAlbums() {
-  const [selectedTab, setSelectedTab] = React.useState(BROWSE);
+  const [selectedOption, setSelectedOption] = React.useState(BROWSE);
 
   return (
     <TopAlbumsProvider>
       <Layout>
-        <div className="tabs is-centered is-medium">
-          <ul>
-            <li
-              className={selectedTab === BROWSE ? 'is-active' : ''}
-              onClick={() => setSelectedTab(BROWSE)}
-            >
-              <a>{BROWSE}</a>
-            </li>
-            <li
-              className={selectedTab === MY_TOP_ALBUMS ? 'is-active' : ''}
-              onClick={() => setSelectedTab(MY_TOP_ALBUMS)}
-            >
-              <a>{MY_TOP_ALBUMS}</a>
-            </li>
-          </ul>
+        <nav
+          className="navbar is-fixed-top has-shadow"
+          role="navigation"
+          aria-label="main navigation"
+        >
+          <div className="navbar-menu">
+            <div className="navbar-start">
+              <a
+                className={`navbar-item ${
+                  selectedOption === BROWSE
+                    ? 'has-text-white has-background-link'
+                    : ''
+                }`}
+                onClick={() => setSelectedOption(BROWSE)}
+              >
+                {BROWSE}
+              </a>
+              <a
+                className={`navbar-item ${
+                  selectedOption === MY_TOP_10_ALBUMS
+                    ? 'has-text-white has-background-link'
+                    : ''
+                }`}
+                onClick={() => setSelectedOption(MY_TOP_10_ALBUMS)}
+              >
+                {MY_TOP_10_ALBUMS}
+              </a>
+            </div>
+          </div>
+        </nav>
+        <br />
+        <div className="mt-6">
+          {selectedOption === BROWSE && <BrowseArtists />}
+          {selectedOption === MY_TOP_10_ALBUMS && (
+            <div className="container">My Top Albums</div>
+          )}
         </div>
-        {selectedTab === BROWSE && <BrowseArtists />}
-        {selectedTab === MY_TOP_ALBUMS && (
-          <div className="container">My Top Albums</div>
-        )}
       </Layout>
     </TopAlbumsProvider>
   );
